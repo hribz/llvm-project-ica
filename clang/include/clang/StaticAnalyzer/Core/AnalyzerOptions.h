6a00f08985eb91ff047355200eab1027891769e6
hribz <1918256943@qq.com>
2024-12-12 17:39:17 +0800

    If we use preprocessed files' diff lines, it's impossible to determine rf during CSA.

--- clang/include/clang/StaticAnalyzer/Core/AnalyzerOptions.h
+++ clang/include/clang/StaticAnalyzer/Core/AnalyzerOptions.h
@@ -196,18 +196,14 @@ public:
   AnalysisPurgeMode AnalysisPurgeOpt = PurgeStmt;
 
   std::string AnalyzeSpecificFunction;
+  /// Just analyze functions in this file.
+  std::string AnalyzeSpecificFunctionFile;
 
   /// File path to which the exploded graph should be dumped.
   std::string DumpExplodedGraphTo;
 
-  /// Just analyze functions in this file.
-  std::string AnalyzeSpecificFunctionFile;
   /// File path to which the AnalysisConsumer::FunctionSummaries should be dumped.
   std::string DumpFSTo;
-  /// File stores diff line info.
-  std::string DiffFile;
-  /// File stores baseline analysis function summaries.
-  std::string FSFile;
 
   /// Store full compiler invocation for reproducible instructions in the
   /// generated report.
